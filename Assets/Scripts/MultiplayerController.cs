87568aca75ed04bf7d4f1d5a0297f96c9370f7c0
wsa138 <wsaddesso@gmail.com>
2024-06-24 22:25:46 -0400

    Add for loop to add players to the game.

--- Assets/Scripts/MultiplayerController.cs
+++ Assets/Scripts/MultiplayerController.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class MultiplayerController : MonoBehaviour
 {
-    public GameObject playerPrefab1; // Reference to the prefab of the player GameObject
-    public GameObject playerPrefab2; // Reference to the prefab of the player GameObject
-    public GameObject playerPrefab3; // Reference to the prefab of the player GameObject
-    public GameObject playerPrefab4; // Reference to the prefab of the player GameObject
-    public Transform spawnPoint1; // Spawn point where players will be instantiated
-    public Transform spawnPoint2; // Spawn point where players will be instantiated
-    public Transform spawnPoint3; // Spawn point where players will be instantiated
-    public Transform spawnPoint4; // Spawn point where players will be instantiated
+    public GameObject[] playerPrefabs; // Reference to the prefab of the player GameObject
+    public Transform[] spawnPoints; // Spawn point where players will be instantiated
+    public int totalPlayers; // Reference to the total number of players that must be spawned.
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("MultiplayerController script started");
-        // Instantiate player prefab at the next spawn point in the array
-        GameObject newPlayer1 = Instantiate(playerPrefab1, spawnPoint1.position, Quaternion.identity);
-        GameObject newPlayer2 = Instantiate(playerPrefab2, spawnPoint2.position, Quaternion.identity);
-        GameObject newPlayer3 = Instantiate(playerPrefab3, spawnPoint3.position, Quaternion.identity);
-        GameObject newPlayer4 = Instantiate(playerPrefab4, spawnPoint4.position, Quaternion.identity);
+
+        // Ensure totalPlayers is within the valid range starting.
+        totalPlayers = Mathf.Clamp(totalPlayers, 1, 4);
+
+        // Loop to instantiate the player prefabs at the spawn points.
+        for (int i = 0; i < totalPlayers; i++)
+        {
+            Instantiate(playerPrefabs[i], spawnPoints[i].position, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
