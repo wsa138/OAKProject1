c09c0632920481f7a1eda71e7f908f25d32aeb14
wsa138 <wsaddesso@gmail.com>
2024-06-30 23:37:52 -0400

    Added cinemachine target group to track all players as they move on camera.

--- Assets/Scripts/MultiplayerController.cs
+++ Assets/Scripts/MultiplayerController.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class MultiplayerController : MonoBehaviour
 {
     public GameObject[] playerPrefabs; // Reference to the prefab of the player GameObject
     public Transform[] spawnPoints; // Spawn point where players will be instantiated
     public int totalPlayers; // Reference to the total number of players that must be spawned.
+    public CinemachineTargetGroup cinemachineTargetGroup; // Reference to the Cinemacine Target Group object.
 
+    [SerializeField] float cinemachineWeight;
+    [SerializeField] float cinemachineRadius;
+     
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +24,10 @@ public class MultiplayerController : MonoBehaviour
         // Loop to instantiate the player prefabs at the spawn points.
         for (int i = 0; i < totalPlayers; i++)
         {
-            Instantiate(playerPrefabs[i], spawnPoints[i].position, Quaternion.identity);
+            GameObject player = Instantiate(playerPrefabs[i], spawnPoints[i].position, Quaternion.identity);
+
+            // Add the instantiated player to the Cinemachine target group.
+            cinemachineTargetGroup.AddMember(player.transform, cinemachineWeight, cinemachineRadius);
         }
     }
 
