4a42c6ffedb14e9a917506ae09e58a7b525fd2db
wsa138 <wsaddesso@gmail.com>
2024-08-11 20:13:06 -0400

    Added sprite arm and rotation.

--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -1,17 +1,26 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerInputHandler : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     private Vector2 moveInput;
     private Rigidbody2D rb;
-    private SpriteRenderer spriteRenderer;
+    private SpriteRenderer[] spriteRenderers;
+    private bool isFlipped = false;
+
+    [SerializeField] Transform arm;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+
     }
 
     private void FixedUpdate()
@@ -19,18 +28,37 @@ public class PlayerInputHandler : MonoBehaviour
         rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
 
         // Check which direction the sprite should be facing and flip accordingly.
-        if (moveInput.x > 0)
+        if (moveInput.x > 0 && isFlipped)
         {
-            spriteRenderer.flipX = true; //Moving right, flip.
+            foreach (SpriteRenderer spriteRenderer in spriteRenderers)
+            {
+                spriteRenderer.flipX = false; //Moving right, flip right.
+                isFlipped = false;
+            }
         } 
-        else if (moveInput.x < 0) 
+        else if (moveInput.x < 0 && !isFlipped) 
         {
-            spriteRenderer.flipX = false; // Moving left, do not flip.
+            foreach (SpriteRenderer spriteRenderer in spriteRenderers)
+            {
+                spriteRenderer.flipX = true; //Moving left, flip left.
+                isFlipped = true;
+            }
         }
     }
 
     void OnMovement(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    void OnAim(InputValue value)
+    {
+        Vector2 aimDirection = value.Get<Vector2>();
+
+        if (aimDirection.sqrMagnitude > 0.01f) // Only rotate if there's significant input
+        {
+            float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+            arm.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 }
