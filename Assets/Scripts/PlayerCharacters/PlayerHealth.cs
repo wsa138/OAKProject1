11057343ca0d0c953b7be22c4b596912b15c91a4
wsa138 <wsaddesso@gmail.com>
2024-09-07 23:58:45 -0400

    Player HealthBar
    When player health hits 0, the health bar fill is set to deactive, and healthbar now looks like it is empty.

--- Assets/Scripts/PlayerCharacters/PlayerHealth.cs
+++ Assets/Scripts/PlayerCharacters/PlayerHealth.cs
@@ -8,16 +8,18 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] FloatingHealthbar healthBar;
 
     private float maxHealth;
+    private GameObject fillArea;
 
     private void Awake()
     {
-
+        fillArea = healthBar.transform.Find("Fill Area").gameObject;
     }
 
     // Start is called before the first frame update
     void Start()
     {
         maxHealth = health;
+
     }
 
     // Update is called once per frame
@@ -34,6 +36,8 @@ public class PlayerHealth : MonoBehaviour
         if (health <=0)
         {
             Debug.Log(gameObject.name + " died");
+
+            fillArea.SetActive(false);
         }
     }
 }
