728ff2e2801b4b3e686e6b857b41b0f3ebcbe07c
wsa138 <wsaddesso@gmail.com>
2024-09-07 23:46:52 -0400

    Added health bar to enemy. Resized health bars for all and changed colors.

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -6,11 +6,21 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int health = 100;
     [SerializeField] GameObject deathEffect;
+    [SerializeField] FloatingHealthbar healthBar;
+
+    private float maxHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = health;
+    }
 
     // Make this enemy take damage
     public void TakeDamage(int damage)
     {
         health -= damage;
+        healthBar.UpdateHealthBar(health, maxHealth);
 
         if (health <=0)
         {
