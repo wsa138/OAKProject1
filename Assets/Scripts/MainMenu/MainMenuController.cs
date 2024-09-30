bf31b6cdd9df0cbf015b05f2ac4f0fe5a335b061
wsa138 <wsaddesso@gmail.com>
2024-09-29 22:52:39 -0400

    Set up more functionality for main menu.

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -1,27 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
     public MultiplayerDataSO multiplayerData; // Access the MultiplayerData Scriptable Object for this project.
 
-    [SerializeField] Button  addPlayerButton;
+    [SerializeField] Button addPlayerButton;
+    [SerializeField] Button removePlayerButton;
+    [SerializeField] Button startGameButton;
+    [SerializeField] TextMeshProUGUI numberOfPlayersText;
 
     // Start is called before the first frame update
     void Start()
     {
-        addPlayerButton.onClick.AddListener(OnButtonClicked);
+        // Set button listeners.
+        addPlayerButton.onClick.AddListener(OnAddPlayerButtonClick);
+        removePlayerButton.onClick.AddListener(OnRemovePlayerButtonClick);
+        startGameButton.onClick.AddListener(OnStartGameButtonClick);
+
+        // Reset the number of players
+        multiplayerData.numberOfPlayers = 1;
     }
 
-    private void OnButtonClicked()
+    private void OnAddPlayerButtonClick()
     {
-        Debug.Log(addPlayerButton.name);
+        // Check if the max number of players is reached. If no, increment.
+        if (multiplayerData.numberOfPlayers == 4)
+        {
+            return;
+        } else
+        {
+            multiplayerData.numberOfPlayers++;
+        }        
+
+        UpdateNumberOfPlayersText();
+        Debug.Log(multiplayerData.numberOfPlayers);
     }
 
+    private void OnStartGameButtonClick()
+    {
+        SceneManager.LoadScene("BattleScene1");
+    }
+
+    private void UpdateNumberOfPlayersText()
+    {
+        numberOfPlayersText.text = multiplayerData.numberOfPlayers.ToString();
+    }
+
+    private void OnRemovePlayerButtonClick()
+    {
+        // Check if the min number of players is reached. If no, decrement.
+        if (multiplayerData.numberOfPlayers == 1)
+        {
+            return;
+        }
+        else
+        {
+            multiplayerData.numberOfPlayers--;
+        }
+
+        UpdateNumberOfPlayersText();
+    }
 }
 
-// Add text showing the multiplayerData.numberOfPlayers
-// Increment number of players when add player button is clicked.
-// When start game button is clicked, move to next screen which should log the total number of players.
